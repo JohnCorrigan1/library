24790d10189e1d384d5d110c662292617442c0c0
John Corrigan <jcorrigan322@gmail.com>
2022-08-19 14:32:47 -0400

    full functionality working needs styling

--- script.js
+++ script.js
@@ -24,12 +24,16 @@ Book.prototype.info = function(){
     return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.isRead
 }
 
-let shining = new Book("shining", "stephen", 200, "read")
-let notShining = new Book("notshining", "notstephen", 100, "not ")
+let shining = new Book("shining", "stephen", 200, "Read")
+let notShining = new Book("notshining", "notstephen", 100, "Not Read")
 
 myLibrary = [shining, notShining]
 
+
+
 const list = document.getElementById("book-list")
+
+
 
 const createRow = function(book, position){
     
@@ -42,7 +46,7 @@ const createRow = function(book, position){
     <td data-position="${position}">${book.title}</td>
     <td>${book.author}</td>
     <td>${book.pages}</td>
-    <td>${book.isRead}</td>
+    <td><button class="read-status">${book.isRead}</button></td>
     <td><button class="delete" type="submit">Remove</td>`;
 
     
@@ -70,12 +74,13 @@ document.getElementById("submit").addEventListener("click", function(e){
 
 
 //iterates through library array and displays books that aren't displayed
-function displayBooks(){
+function displayBooks(position){
     for(i = 0; i < myLibrary.length; i++){
         if(!myLibrary[i].onPage){
             createRow(myLibrary[i], i)
             myLibrary[i].onPage = true;
         }
+        
     }
 }
 
@@ -83,13 +88,37 @@ function displayBooks(){
 
 //deletes row of book
 list.addEventListener("click", e => {
+    //index of row clicked
+    let tableBody = e.target.parentElement.parentElement.parentElement;
+    let bookIndex = e.target.parentElement.parentElement.dataset.position;
+    //console.log(bookIndex);
     if(e.target.classList.contains('delete')){
-        e.target.parentElement.parentElement.remove();
         myLibrary.splice(e.target.parentElement.parentElement.dataset.position, 1)
+
+        while(tableBody.firstChild){
+            //console.log(tableBody.lastChild.dataset.position)
+            console.log("test")
+           tableBody.removeChild(tableBody.lastChild)
+        }
+        //e.target.parentElement.parentElement.remove();
+        //console.log(row.children)
+        for(i = 0; i < myLibrary.length; i++){
+            myLibrary[i].onPage = false;
+        }
         displayBooks();
     }
-})
 
+    if(e.target.classList.contains('read-status')){
+        if(e.target.innerHTML == "Read"){
+            e.target.innerHTML = "Not Read"
+            myLibrary[bookIndex].isRead = "Not Read"
+        }
+        else if(e.target.innerHTML == "Not Read"){
+            e.target.innerHTML = "Read"
+            myLibrary[bookIndex].isRead = "Read"
+        }   
+    }    
+})
 
 
 displayBooks();
